5d57a10a95a213268c36adc263b7696be2ad609e
Salah GHAMIZI <salah.ghamizi@gmail.com>
2021-11-04 11:50:26 +0100

    Face Api js V1

--- client/src/components/EmotionGame/index.tsx
+++ client/src/components/EmotionGame/index.tsx
@@ -1,149 +1,70 @@
-import React, {Component} from "react";
+import React, { useRef, useState, Component} from "react";
 import { IonGrid, IonRow, IonCol } from '@ionic/react';
 import './style.scss';
 
 import {GridProps, BoxProps, EmotionGamePropsType} from "../../types/types";
-import faces from '../../data/faces.json';
+import { useFaceApi, } from 'react-use-faceapi';
+
 import Confetti from "react-confetti";
 
-
-function arrayClone(arr) {
-	return JSON.parse(JSON.stringify(arr));
+const myFaceApiConfig = {
+    input: 'my-image',
+    refreshRate: 250,
+	useFaceExpressionModel:true
 }
 
-class Box extends Component<BoxProps> {
-	selectBox = () => {
-		this.props.selectBox(this.props.row, this.props.col);
-	}
+function FaceRecognition({nbSteps}) {
+	// Attributes
+    const imageRef = useRef();
+    const [image, setImage] = useState("");
+    var faces = useFaceApi(myFaceApiConfig);
 
-	render() {
-		return(
-			<div className={this.props.boxClass}
-				id={this.props.id}
-				onClick={this.selectBox}
-				 title={this.props.face}
-				 style={{ backgroundImage: `url(${"../../assets/imgs/"+this.props.face.replace("#","/") || ""})` }}
-			/>
-		)
-	}
-}
+    // Rendering
+    return (
+        <div>
+
+            <img style={{ width: 500 }} ref={imageRef} id="my-image" src={image} />
+            <input type="file" onChange={(event) => setImage(URL.createObjectURL(event.target.files[0]))} />
+			{faces.length}
+            {faces && faces.map((face) => {
+
+                // Attributes
+                // @ts-ignore
+				const top = face.relativeBox.top * imageRef.current.offsetHeight;
+				// @ts-ignore
+                const left = face.relativeBox.left * imageRef.current.offsetWidth;
+                // @ts-ignore
+                const width = face.relativeBox.width * imageRef.current.offsetWidth;
+                // @ts-ignore
+                const height = face.relativeBox.height * imageRef.current.offsetHeight;
 
 
-class Grid extends Component<GridProps> {
-	render() {
-		const width = (this.props.cols * 122) + 1;
-		var boxArr = [];
-		var gridBoxClass = "";
+                // Rendering
+                return (
+                    <div style={{
+                        position: 'absolute',
+                        width: width,
+                        height: height,
+                        left: left,
+                        top: top,
+                        border: '1px solid red',
+                    }}
+                    />
+                );
+            })}
 
-
-		if (this.props.ranFaces){
-			for (var j = 0; j < this.props.rows; j++) {
-			for (var i = 0; i < this.props.cols; i++) {
-				let gridBoxId = i + "_" + j;
-				if (this.props.gridFull[j][i]=="C"){
-					gridBoxClass = "box on"
-				}
-				else if (this.props.gridFull[j][i]=="F"){
-					gridBoxClass = "box off"
-				}
-				else if (this.props.gridFull[j][i]=="N"){
-					gridBoxClass = "box no"
-				}
-				boxArr.push(
-					<Box
-						boxClass={gridBoxClass}
-						key={gridBoxId}
-						id={gridBoxId}
-						row={j}
-						col={i}
-						face={this.props.ranFaces[i][j]}
-						selectBox={this.props.selectBox}
-					/>
-				)
-		    }
-		}
-
-		}
-
-		return(
-			<div className="grid" style={{ width: width}}>
-				{boxArr}
-			</div>
-
-		)
-	}
+        </div >
+    )
 }
 
 class EmotionGame extends Component<EmotionGamePropsType> {
 	state: any;
 
-
-
 	constructor(props: any) {
 		super(props);
 
-
-		const minAge = 21;
-		const maxAge = 60;
-		var ages = Array.from(Array(maxAge-minAge).keys()).map(function(val){return val+minAge;});
-		var ranAges = {},
-			i = this.props.cols*2,
-			j = 0;
-
-		while (i--) {
-			j = Math.floor(Math.random() * (ages.length));
-			ranAges[ages[j]] = [];
-			ages.splice(j,1);
-		}
-
-		for(i=0;i<faces.length;i++){
-
-			let face_age = faces[i].split("#")[1].split("-")[0];
-			let true_age = faces[i].split("#")[0].split("-")[1];
-			if (face_age in ranAges) {
-				//if (faces[i]!="age-"+29+"#"+29"+-M.jpg")
-				if(face_age !=true_age){
-					ranAges[face_age].push(faces[i])
-				}
-
-			}
-		}
-
-		var ranFaces = []
-		var trueAges = []
-
-		for (const [key, values] of Object.entries(ranAges)) {
-			var vals:any =values;
-			if (vals.length>this.props.cols && Object.keys(ranFaces).length<this.props.cols){
-
-				i = this.props.rows - 1;
-				let advFaces = []
-
-				while (i--) {
-					j = Math.floor(Math.random() * (vals.length));
-					if (vals[j] != undefined) {
-						advFaces.push(vals[j]);
-						vals.splice(j, 1);
-					}
-				}
-				if (Math.random() > 0.5){
-					advFaces.push("age-"+key+"#"+key+"-F.jpg");
-				}
-				else{
-					advFaces.push("age-"+key+"#"+key+"-M.jpg");
-				}
-
-				trueAges.push(key);
-				ranFaces.push(advFaces.sort(() => Math.random() - 0.5));
-
-			}
-
-		}
 		this.state = {
 			score : 0,
-			gridFull: Array(this.props.rows).fill([]).map(() => Array(this.props.cols).fill(false)),
-			ranFaces: ranFaces,
-			trueAges: trueAges,
 			selectedFace:"",
 			selectedColumn:0,
 			startDate:Date.now(),
@@ -151,88 +72,14 @@ class EmotionGame extends Component<EmotionGamePropsType> {
 			width : window.innerWidth
 		}
 
-		if (this.props.db){
-			const doc = this.props.db.collection('llc')
-			console.log(doc)
-			const observer = doc.where('game', '==', 'AGE').where('type', '==', 'QR')
-			  .onSnapshot(querySnapshot => {
-				querySnapshot.docChanges().forEach(change => {
-
-				if (change.type === "added") {
-					let params = change.doc.data().params;
-					let time = change.doc.data().time*1000;
-					console.log(time, this.state.startDate, time > this.state.startDate)
-					if (time > this.state.startDate){
-						this.selectBox(parseInt(params[0]),parseInt(params[1]))
-					}
-
-				}
-			  });
-			});
-		}
-
-		//alert("store grid")
-
 	}
 
 	selectBox = (row, col) => {
-		let selectedColumn = this.state.selectedColumn
-
-		if (col!=selectedColumn){
-			return
-		}
-
-		let gridCopy = arrayClone(this.state.gridFull);
-		///if (gridCopy[row][col]) {
-		//	gridCopy[row][col] = !gridCopy[row][col];
-		//}
-
-		let selectedFace = this.state.ranFaces[this.state.selectedColumn][row]
-		console.log(selectedFace,this.state.selectedFace, selectedFace!=this.state.selectedFace)
-
-		if (selectedFace != this.state.selectedFace){
-			this.setState({
-				//gridFull: gridCopy,
-				selectedFace : selectedFace
-			})
-		}
-
-		else{
-
-			let face = selectedFace.split("-")[1].split("#")
-			console.log(face)
-			if (face[0]!=face[1]){
-				gridCopy[row][col] = "N"
-			}
-			else{
-				for (var i=0;i<gridCopy.length;i++){
-					if (i!=row){
-						gridCopy[i][col] = "N"
-					}
-				}
-				selectedColumn++
-			}
-			this.setState({gridFull:gridCopy,selectedColumn:selectedColumn})
-		}
-
 
 	}
 
 	populate = () => {
 
-		let gridCopy = arrayClone(this.state.gridFull);
-		for (let j = 0; j < this.props.cols; j++) {
-			for (let i = 0; i < this.props.rows; i++) {
-				let face = this.state.ranFaces[j][i].split("-")[1].split("#")
-				gridCopy[i][j] = face[0]==face[1]?"C":"F";
-			}
-		}
-
-
-		this.setState({
-			gridFull: gridCopy,
-
-		})
 	}
 
 
@@ -243,45 +90,7 @@ class EmotionGame extends Component<EmotionGamePropsType> {
 
 	render() {
 		return (
-			<IonGrid>
-				{this.state.selectedColumn==this.state.trueAges.length && <Confetti width={this.state.width} height={this.state.height} />}
-			  <IonRow>
-				<IonCol><div>
-						<div className="grid" style={{ width: this.state.trueAges.length * 122}}>
-							{this.state.trueAges.map(function(age, index){
-								return <div style={{ textAlign: "center"}} className={"box"} key={ index }>
-									<span style={{ display: "inline-block", marginTop:60}}>{age}</span>
-							</div>;
-							  })}
-						</div>
-
-					  <Grid rows={this.props.rows} cols={this.props.cols} gridFull={this.state.gridFull} selectBox={this.selectBox} ranFaces={this.state.ranFaces}></Grid>
-					</div></IonCol>
-				<IonCol style={{textAlign: "center"}}>
-					<h3>Selected Face</h3>
-
-					{this.state.selectedFace &&
-						<div>
-							<div className="box large"
-								style={{ backgroundImage: `url(${"../../assets/imgs/"+this.state.selectedFace.replace("#","/") || ""})` }}
-							/>
-
-							{this.state.selectedColumn<this.state.trueAges.length &&
-							<p>Scan again to confirm that this person is {this.state.trueAges[this.state.selectedColumn]} years old ;)</p>
-							}
-
-							{this.state.selectedColumn==this.state.trueAges.length &&
-							<p>Congratulations! You completed the game</p>
-							}
-
-						</div>
-
-					}
-				</IonCol>
-			  </IonRow>
-			</IonGrid>
-
-
+			<FaceRecognition nbSteps={0}></FaceRecognition>
 		);
 	};
 }
