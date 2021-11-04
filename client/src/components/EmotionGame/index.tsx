1df4ac6c2213efbee3e3ca79513ba96bd4b56730
Salah GHAMIZI <salah.ghamizi@gmail.com>
2021-11-04 14:31:05 +0100

    Fixed icon dependencies

--- client/src/components/EmotionGame/index.tsx
+++ client/src/components/EmotionGame/index.tsx
@@ -5,7 +5,6 @@ import {loadModels} from "../../utils/faceApi";
 import {createFaLibrary} from "../../utils/icons";
 
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import Switch from "react-switch";
 import Camera from "../../components/Camera/Camera";
 import {EmotionGamePropsType} from "../../types/types";
 
@@ -25,7 +24,7 @@ class EmotionGame extends Component<EmotionGamePropsType> {
 			startDate:Date.now(),
 			height : window.innerHeight,
 			width : window.innerWidth,
-			mode: true
+			mode: false
 		}
 
 	}
@@ -41,19 +40,9 @@ class EmotionGame extends Component<EmotionGamePropsType> {
 			  <header>
 				<div className="App__header">
 				  <h1>
-					<span>Mood Detector</span>
+					<span>Emotion Detector</span>
 				  </h1>
-				  <div className="App__switcher">
-					<FontAwesomeIcon
-					  icon="camera"
-					  color={this.state.mode ? "#007c6c" : "#cccccc"}
-					/>
 
-					<FontAwesomeIcon
-					  icon="video"
-					  color={!this.state.mode ? "#007c6c" : "#cccccc"}
-					/>
-				  </div>
 				</div>
 			  </header>
 			  <Camera photoMode={this.state.mode} />
