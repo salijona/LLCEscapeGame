f44befe668af74bcd4687cfb8c1852a3b8854f39
Salah GHAMIZI <salah.ghamizi@gmail.com>
2022-03-16 18:27:58 +0100

    Added Tweet Game

--- client/src/components/TweetGame/index.tsx
+++ client/src/components/TweetGame/index.tsx
@@ -79,9 +79,10 @@ class TweetGame extends Component<TweetGamePropsType> {
 				this.setState({"time":time})
 			}
 
-			this.setState({"selectedColumn":this.state.selectedColumn+1,"correct":correct, "nbClick":this.state.nbClick+1})
+			this.setState({"selectedColumn":this.state.selectedColumn+1,"correct":correct})
 
 		}
+		this.setState({"nbClick":this.state.nbClick+1})
 		this.updateTweets()
 
 
@@ -113,6 +114,11 @@ class TweetGame extends Component<TweetGamePropsType> {
 				<div>
 					<Confetti width={this.state.width} height={this.state.height} />
 					<p className="scoreFinal">Game Over. Your finished the game in {this.state.time}s and {this.state.nbClick} clicks</p>
+					{this.state.time<=180 && this.state.nbClick<10 &&
+					<p className="scoreFinal">Congratulations! You collected all the tweets! The secret code is <strong>31216</strong></p>}
+
+					{this.state.time>180 || this.state.nbClick>=10 &&
+					<p className="scoreFinal">Too bad, you took too much time. The system has been reset Try again!</p>}
 				</div>
 
 				}
